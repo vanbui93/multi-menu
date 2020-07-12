80bc32f176ae34bff1273092c7088728fc3ef31a
vanbui93 <van.bt.38@gmail.com>
2020-07-12 18:41:03 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,62 +1,64 @@
 import React, { Component } from 'react'
+import Nav from './Components/Nav';
 
 const menuList = [
   {
     menu: "Menu 1",
+    link: 'link-1',
     submenu: [
       {
-        menu: "Menu 2",
+        menu: "Sub Menu 2.1",
+        link: 'link-2.1',
         submenu: [
           {
-            menu: "Menu 3",
-            submenu: [
-              {
-                menu: "Menu 4",
-              }
-            ]
-          }
+            menu: "Sub Menu 3.1",
+          },
+          {
+            menu: "Sub Menu 3.2",
+          },
         ]
       },
       {
-        menu: "About"
+        menu: "Sub Menu 2.2",
+        link: 'link-2.2',
+        submenu: [
+          {
+            menu: "Sub Menu 3",
+            link: 'link-sub-3',
+          }
+        ]
       }
     ]
   },
   {
-    menu: "Liverpool",
+    menu: "Menu 2",
+    link: 'link-2',
     submenu: [
       {
-        menu: "Menu 2",
+        menu: "Sub Menu 2",
+        link: 'link-sub-2',
         submenu: [
           {
-            menu: "Menu 3"
-          }
+            menu: "Sub Menu 3.1",
+            link: 'link-sub-3.1',
+          },
+          {
+            menu: "Sub Menu 3.2",
+            link: 'link-sub-3.2',
+          },
         ]
       }
     ]
   }
 ];
 
-const Dropdown = ({ menuList }) => {
-  const renderMenu = items => {
-    return items.map((item, index) => {
-      return (
-        <div style={{ marginLeft: 10 }} key={index}>
-          {item.menu}
-          {item.submenu ? renderMenu(item.submenu) : null}
-        </div>
-      );
-    });
-  };
 
-  return <div>{renderMenu(menuList)}</div>;
-};
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Dropdown menuList={menuList} />
+        <Nav menuList={menuList} />
       </div>
     )
   }
--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-import './styles.scss'
+import './styles.scss';
+
+const Dropdown = ({ menuList }) => {
+  const renderMenu = items => {
+    return items.map((item, index) => {
+      return (
+        <li key={index}>
+          <a href={(item.link) ? item.link : ''}>{item.menu}</a>
+          <ul>
+            {item.submenu ? renderMenu(item.submenu) : null}
+          </ul>
+        </li>
+      );
+    });
+  };
+
+  return <ul className="dropdown-menu">{renderMenu(menuList)}</ul>;
+};
 
 class Nav extends Component {
-    render() {
-        return (
-            <div></div>
-        )
-    }
+
+  render() {
+    const { menuList } = this.props;
+    return (
+      <div>
+        {Dropdown({ menuList })}
+      </div>
+    )
+  }
 }
+
 export default Nav;
